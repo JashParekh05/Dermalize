3efa7c74e323e2f4a199ea1bf2a862a3cfe3f19b
Saahil Khanna <saahilkhanna100@gmail.com>
2023-10-14 07:13:12 -0400

    Bitch ass react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,28 @@
+// src/App.js
+
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar'; // Import the Navbar component
+import Navbar from './Components/Navbar';
+import About from './Components/AboutUs'; // Import the About component
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar /> {/* Include the Navbar component here */}
-      <div className="content">
-        {/* Your main content goes here */}
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/about" component={About} /> {/* Define the route for "About Us" */}
+            <Route path="/" exact>
+              {/* Define the route for the home page */}
+              <h1>Home Page</h1>
+              <p>This is the home page content.</p>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,14 +1,17 @@
 // src/Navbar.js
 
 import React from 'react';
-import './Navbar.css'; // We'll create this CSS file for styling
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li className="nav-item"><a href="/">Home</a></li>
-        <li className="nav-item"><a href="/about">About</a></li>
+        <li className="nav-item"><Link to="/">Home</Link></li> {/* Use Link for navigation */}
+        <li className="nav-item"><Link to="/about">About</Link></li> {/* Use Link for navigation */}
+        <li className="nav-item"><a href="/services">Services</a></li>
+        <li className="nav-item"><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
