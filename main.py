281c98318097dbdbe7df2d1029a3aeda5470b837
RishiBappanad <65369047+RishiBappanad@users.noreply.github.com>
2023-10-14 01:49:46 -0400

    flask main update

--- main.py
+++ main.py
@@ -37,5 +37,4 @@ def upload_image():
     return jsonify({'error': 'Unexpected error'})
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run()
