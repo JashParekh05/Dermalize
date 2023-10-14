1bbc14cbee19fb8ea5dc9a9ea737c946e66177ff
JashParekh05 <jash.parekh05@gmail.com>
2023-10-14 06:03:58 -0400

    New Changes

--- Backend/model.py
+++ Backend/model.py
@@ -1,13 +1,17 @@
-import torch
 import torch.nn as nn
 from torchvision import models
 
+# Define the model architecture
 class SkinNet(nn.Module):
     def __init__(self, num_classes):
         super(SkinNet, self).__init__()
-        self.network = models.resnet50(pretrained=True)
+
+        # Use a pretrained ResNet model
+        self.network = models.resnet50()  # Remove 'pretrained=True'
+
+        # Replace the last fully connected layer for our num_classes
         num_ftrs = self.network.fc.in_features
-        self.network.fc = nn.Linear(num_ftrs, num_classes)  # Correct the arguments
+        self.network.fc = nn.Linear(num_ftrs, num_classes)
 
     def forward(self, x):
         return self.network(x)
