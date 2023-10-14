caf3647ee7fd574943e985d573d15d167f446f20
Saahil Khanna <saahilkhanna100@gmail.com>
2023-10-14 07:00:42 -0400

    Pushing website changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/menu" exact component={Menu} />
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
         </Switch>
-        <Footer />
       </Router>
     </div>
   );
