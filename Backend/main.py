2d57e29d779e28480aa0a9336b33e7b48cb2749d
JashParekh05 <jash.parekh05@gmail.com>
2023-10-15 01:02:06 -0400

    Fixed test and train

--- Backend/main.py
+++ Backend/main.py
@@ -1,123 +1,283 @@
-<<<<<<< Updated upstream
-import os
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import transforms, datasets
-from model import SkinNet  # Import the model from model.py
-
-# Define data directories
-train_dir = '/Users/jashparekh/Documents/GitHub/Dermalize/Backend/dermnet/train'
-test_dir = '/Users/jashparekh/Documents/GitHub/Dermalize/Backend/dermnet/test'
-
-# Hyperparameters
-num_classes = 23
-learning_rate = 0.001
-batch_size = 64
-num_epochs = 10
-
-# Image transforms
-data_transforms = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-=======
-
-# import the necessary packages
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-import numpy as np
-import argparse
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import seaborn as sns
+import random
+from PIL import Image
+from keras.preprocessing import image
+from keras.applications.inception_v3 import InceptionV3
+from keras import Sequential, Model
+from keras.layers import (Conv2D, MaxPooling2D, Dense, Flatten, \
+Dropout, Input,GlobalAveragePooling2D,BatchNormalization)
+from keras.activations import softmax
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from mpl_toolkits.axes_grid1 import ImageGrid
+from keras.models import Model
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.metrics import (ConfusionMatrixDisplay, confusion_matrix,
+accuracy_score)
 import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import seaborn as sns
+import random
+from PIL import Image
+import keras
+from keras.preprocessing import image
+from keras.applications.inception_v3 import InceptionV3
+from keras import Sequential, Model
+from keras.layers import (Conv2D, MaxPooling2D, Dense, Flatten,
+Dropout, Input,GlobalAveragePooling2D,BatchNormalization)
+from keras.activations import softmax
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from mpl_toolkits.axes_grid1 import ImageGrid
+from keras.models import Model
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.metrics import (ConfusionMatrixDisplay, confusion_matrix, accuracy_score)
+# Base Libraries
+from keras.applications import VGG19,EfficientNetB0,VGG16,InceptionV3,ResNet50,EfficientNetB3
+from keras.models import Model
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
-from matplotlib import pyplot as plt
+# Transformation
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import power_transform
+from sklearn.pipeline import Pipeline
+# Feature Selection
 
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
-prototxtPath = "/Users/jashparekh/Documents/GitHub/Dermalize/Backend/deploy.prototxt"
-weightsPath = "/Users/jashparekh/Documents/GitHub/Dermalize/Backend/res10_300x300_ssd_iter_140000.caffemodel"
->>>>>>> Stashed changes
+import os
 
-# load our serialized face detector model from disk
-print("[INFO] loading face detector model...")
-prototxtPath = os.path.sep.join(["face_detector", "deploy.prototxt"])
-weightsPath = os.path.sep.join(["face_detector",
-	"res10_300x300_ssd_iter_140000.caffemodel"])
-net = cv2.dnn.readNet(prototxtPath, weightsPath)
+train2=os.listdir("/Users/mokshith/Downloads/archive/train")
 
-# load the face mask detector model from disk
-print("[INFO] loading face acne detector model...")
-model = load_model("/facemodel.model")
 
-# load the input image from disk, clone it, and grab the image spatial
-# dimensions
-image = cv2.imread("images.jpg")
-orig = image.copy()
-(h, w) = image.shape[:2]
+train2
 
-# construct a blob from the image
-blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
-	(104.0, 177.0, 123.0))
+cat2=['Melanoma Skin Cancer Nevi and Moles']
 
-# pass the blob through the network and obtain the face detections
-print("[INFO] computing face detections...")
-net.setInput(blob)
-detections = net.forward()
+cat=['Acne and Rosacea Photos',
+'Normal',
+'vitiligo',
+'Tinea Ringworm Candidiasis and other Fungal Infections',
+'Melanoma Skin Cancer Nevi and Moles',
+'Eczema Photos']
 
-# loop over the detections
-for i in range(0, detections.shape[2]):
-	# extract the confidence (i.e., probability) associated with
-	# the detection
-	confidence = detections[0, 0, i, 2]
 
-	# filter out weak detections by ensuring the confidence is
-	# greater than the minimum confidence
-	if confidence > 0.5:
-		# compute the (x, y)-coordinates of the bounding box for
-		# the object
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
+def data_dictionary():
+path_train="/Users/mokshith/Downloads/skin/train/"
+path_test="/Users/mokshith/Downloads/archive/test/"
+list_train=cat#os.listdir(path_train)
+list_train1=cat2
+train_dictionary={"image_path":[],"target":[]}
+test_dictionary={"image_path":[],"target":[]}
+k=0
+for i in list_train:
+path_disease_train=path_train+i
+image_list_train=os.listdir(path_disease_train)
+for j in image_list_train:
+img_path_train=path_disease_train+"/"+j
+if img_path_train!="/Users/mokshith/Downloads/skin/train/Normal/34.avif":
+train_dictionary["image_path"].append(img_path_train)
+train_dictionary['target'].append(k)
+k+=1
+for i in list_train1:
+path_disease_test=path_test+i
+image_list_test=os.listdir(path_disease_test)
+for j in image_list_test:
+img_path_train=path_disease_test+"/"+j
+train_dictionary["image_path"].append(img_path_train)
+train_dictionary['target'].append(4)
+train_df=pd.DataFrame(train_dictionary)
+return train_df
 
-		# ensure the bounding boxes fall within the dimensions of
-		# the frame
-		(startX, startY) = (max(0, startX), max(0, startY))
-		(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-		# extract the face ROI, convert it from BGR to RGB channel
-		# ordering, resize it to 224x224, and preprocess it
-		face = image[startY:endY, startX:endX]
-		face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-		face = cv2.resize(face, (224, 224))
-		face = img_to_array(face)
-		face = preprocess_input(face)
-		face = np.expand_dims(face, axis=0)
+train=data_dictionary()
 
-		# pass the face through the model to determine if the face
-		# has a acne or not
-		(acne, withoutAcne) = model.predict(face)[0]
+train.info()
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label = "Acne" if acne > withoutAcne else "No Acne"
-		color = (0, 0, 255) if label == "Acne" else (0, 255, 0)
+import cv2
+images=[]
+label=[]
+for i in train['image_path']:
+if i!="/Users/mokshith/Downloads/skin/train/Normal/34.avif":
+img=cv2.imread(i)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img=cv2.resize(img,(180,180))
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#img=cv2.resize(img,(64,64),interpolation=cv2.INTER_AREA)
+#img=resize_and_rescale(img)
+images.append(img)
 
-		# include the probability in the label
-		label = "{}: {:.2f}%".format(label, max(acne, withoutAcne) * 100)
+example_list = [image for image in random.sample(images, k=6)]
 
-<<<<<<< Updated upstream
-print(f'Accuracy on the test set: {100 * correct / total}%')
-=======
-		# display the label and bounding box rectangle on the output
-		# frame
-		cv2.putText(image, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+# Plot the images in a 2x3 grid
+fig, axes = plt.subplots(2, 3, figsize=(10, 8))
 
-# show the output image
-plt.imshow((cv2.cvtColor(image, cv2.COLOR_BGR2RGB)))
+for ax, img_array in zip(axes.ravel(), example_list):
+ax.imshow(img_array)
+ax.axis('off')
+
 plt.show()
->>>>>>> Stashed changes
+
+label=train['target']
+
+len(label)
+
+len(images)
+
+import numpy as np
+import pandas as pd
+
+data=np.array(images)
+
+label=np.array(label)
+
+data.shape
+
+label.shape
+
+vgg_model = VGG19(weights = 'imagenet', include_top = False, input_shape = (180, 180, 3))
+
+for layer in vgg_model.layers :
+layer.trainable = False
+
+from keras.utils import plot_model
+plot_model(vgg_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D,Flatten, Dense
+
+model=Sequential([
+Dense(200,activation='relu'),
+Dense(170,activation='relu'),
+Dense(6,activation='softmax'),
+
+])
+
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+
+mcp_save = ModelCheckpoint('EnetB0_CIFAR10_TL.h5', save_best_only=True, monitor='val_acc')
+reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=2, verbose=1,)
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+kf = KFold(n_splits = 3)
+
+import keras
+acc=[]
+num_classes = 6
+label = keras.utils.to_categorical(label, num_classes)
+
+label.shape
+
+for i in range(3):
+x_train,x_test, y_train, y_test=train_test_split(data, label, test_size=0.2,random_state = np.random.randint(1,1000, 1)[0])
+print(x_train.shape)
+# let's make all layers non-trainable
+for layer in vgg_model.layers :
+layer.trainable = False
+features_train=vgg_model.predict(x_train)
+features_test=vgg_model.predict(x_test)
+print(features_train.shape)
+num_train=x_train.shape[0]
+num_test=x_test.shape[0]
+print(num_train)
+print(num_test)
+x_test=features_test.reshape(num_test,-1)
+x_train=features_train.reshape(num_train,-1)
+print(x_train.shape)
+print(x_train.shape)
+#x_train,x_valid,y_train,y_valid= train_test_split(x_train, y_train, test_size=0.1,random_state = np.random.randint(1,1000, 1)[0])
+# history=model.fit(x_train,y_train,epochs=15,batch_size=32,verbose=1)
+# model.evaluate(x_test,y_test)
+
+## fuck u bitch history = model.fit(x_train, y_train, epochs=1)
+
+
+model.save('6claass.h5')
+
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+y_pred=model.predict(x_test)
+
+y_pred2=np.argmax(y_pred, axis=1)
+y_pred2
+
+
+y_test=np.argmax(y_test, axis=1)
+
+y_test
+
+confusion_matrix_result=confusion_matrix(y_test,y_pred2)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pylab as pl
+plt.title("skin detection using VGG19 ")
+
+ax= plt.subplot()
+sns.heatmap(confusion_matrix_result, annot=True, fmt='g', ax=ax);
+pl.show()
+
+
+train_accuracy = history.history['accuracy']
+#val_accuracy = history.history['val_accuracy']
+train_loss = history.history['loss']
+#val_loss = history.history['val_loss']
+
+epochs = range(len(train_accuracy))
+plt.figure(figsize=(12,4))
+# Plotting the accuracy
+plt.subplot(1,2,1)
+plt.plot(epochs, train_accuracy, 'b', label='Training accuracy')
+
+plt.title('accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['train', 'val'], loc='lower right')
+
+# Plotting the loss
+plt.subplot(1,2,2)
+plt.plot(epochs, train_loss, 'b', label='Training loss')
+
+plt.title('Loss ')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(['train', 'val'], loc='upper right')
+
+import os
+import matplotlib.pyplot as plt
+import cv2
+from keras.models import Model, Sequential
+from keras.layers import Input, Convolution2D, ZeroPadding2D,MaxPooling2D, Flatten, Dense, Dropout, Activation
+from PIL import Image
+import numpy as np
+from keras.preprocessing.image import load_img, save_img, img_to_array
+from keras.applications.imagenet_utils import preprocess_input
+from keras.preprocessing import image
+import pandas as pd
+import tensorflow as tf
+import keras
+import pickle
+
+confusion_matrix=classification_report(y_test,y_pred2)
+
+print(confusion_matrix)
+
+
+
+
